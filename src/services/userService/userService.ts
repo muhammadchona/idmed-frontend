d425b8d1c567e945d8e13ae1db5c3d33cf4d2362
Joao Nuno Mabjaia <94103183+joaonunomabjaia@users.noreply.github.com>
2023-05-08 15:41:20 +0200

    Parametrizacao Update

---
+++ src/services/userService/userService.ts
@@ -0,0 +1,112 @@
+import { useRepo } from 'pinia-orm';
+import User from "src/stores/models/userLogin/User";
+import api from '../apiService/apiService';
+import { alert } from 'src/components/Shared/Dialog/Dialog';
+
+const user = useRepo(User);
+
+export default {
+  post(params: string) {
+    return api()
+      .post('user', params)
+      .then((resp) => {
+        user.save(resp.data);
+        alert(
+          'Sucesso!',
+          'O Registo foi efectuado com sucesso',
+          null,
+          null,
+          null
+        );
+      })
+      .catch((error) => {
+        if (error.request != null) {
+          const arrayErrors = JSON.parse(error.request.response);
+          const listErrors = [];
+          if (arrayErrors.total == null) {
+            listErrors.push(arrayErrors.message);
+          } else {
+            arrayErrors._embedded.errors.forEach((element) => {
+              listErrors.push(element.message);
+            });
+          }
+          alert('Erro no registo', listErrors, null, null, null);
+        } else if (error.request) {
+          alert('Erro no registo', error.request, null, null, null);
+        } else {
+          alert('Erro no registo', error.message, null, null, null);
+        }
+      });
+  },
+  get(offset: number) {
+    if (offset >= 0) {
+      return api()
+        .get('user?offset=' + offset + '&limit=100')
+        .then((resp) => {
+          user.save(resp.data);
+          offset = offset + 100;
+          if (resp.data.length > 0) {
+            this.get(offset);
+            setTimeout(this.get, 2)
+          }
+        })
+        .catch((error) => {
+          if (error.request != null) {
+            const arrayErrors = JSON.parse(error.request.response);
+            const listErrors = {};
+            if (arrayErrors.total == null) {
+              listErrors.push(arrayErrors.message);
+            } else {
+              arrayErrors._embedded.errors.forEach((element) => {
+                listErrors.push(element.message);
+              });
+            }
+            alert('Erro no registo', listErrors, null, null, null);
+          } else if (error.request) {
+            alert('Erro no registo', error.request, null, null, null);
+          } else {
+            alert('Erro no registo', error.message, null, null, null);
+          }
+        });
+    }
+  },
+  patch(id: number, params: string) {
+    return api()
+      .patch('user/' + id, params)
+      .then((resp) => {
+        user.save(resp.data);
+        alert(
+          'Sucesso!',
+          'O Registo foi alterado com sucesso',
+          null,
+          null,
+          null
+        );
+      })
+      .catch((error) => {
+        if (error.request != null) {
+          const arrayErrors = JSON.parse(error.request.response);
+          const listErrors = {};
+          if (arrayErrors.total == null) {
+            listErrors.push(arrayErrors.message);
+          } else {
+            arrayErrors._embedded.errors.forEach((element) => {
+              listErrors.push(element.message);
+            });
+          }
+          alert('Erro no registo', listErrors, null, null, null);
+        } else if (error.request) {
+          alert('Erro no registo', error.request, null, null, null);
+        } else {
+          alert('Erro no registo', error.message, null, null, null);
+        }
+      });
+  },
+  delete(id: number) {
+    return api()
+      .delete('user/' + id)
+      .then(() => {
+        user.destroy(id);
+      });
+  },
+};
