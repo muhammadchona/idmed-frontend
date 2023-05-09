d134dd82aa048dc22ea368ff74f908e4e0637e99
colaconhongo <colaco.nhango@fgh.org.mz>
2023-05-09 13:58:01 +0200

    Update Services API

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,9 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
+    API_URL: string;
+    IDMED_PROV_SERVER: string;
+    IDART_PROV_SERVER: string;
     NODE_ENV: string;
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
     VUE_ROUTER_BASE: string | undefined;
---
+++ src/services/api/identifierTypeService/identifierTypeService.ts
@@ -0,0 +1,113 @@
+
+import { useRepo } from 'pinia-orm';
+import { IdentifierType } from 'src/stores/models/identifierType/IdentifierType';
+import api from '../apiService/apiService';
+import { alert } from 'src/components/Shared/Dialog/Dialog';
+
+const identifierType = useRepo(IdentifierType);
+
+export default {
+  post(params: string) {
+    return api()
+      .post('identifierType', params)
+      .then((resp) => {
+        identifierType.save(resp.data);
+        alert(
+          'Sucesso!',
+          'O Registo foi efectuado com sucesso',
+          null,
+          null,
+          null
+        );
+      })
+      .catch((error) => {
+        if (error.request != null) {
+          const arrayErrors = JSON.parse(error.request.response);
+          const listErrors = [];
+          if (arrayErrors.total == null) {
+            listErrors.push(arrayErrors.message);
+          } else {
+            arrayErrors._embedded.errors.forEach((element) => {
+              listErrors.push(element.message);
+            });
+          }
+          alert('Erro no registo', listErrors, null, null, null);
+        } else if (error.request) {
+          alert('Erro no registo', error.request, null, null, null);
+        } else {
+          alert('Erro no registo', error.message, null, null, null);
+        }
+      });
+  },
+  get(offset: number) {
+    if (offset >= 0) {
+      return api()
+        .get('identifierType?offset=' + offset + '&limit=100')
+        .then((resp) => {
+          identifierType.save(resp.data);
+          offset = offset + 100;
+          if (resp.data.length > 0) {
+            this.get(offset);
+            setTimeout(this.get, 2)
+          }
+        })
+        .catch((error) => {
+          if (error.request != null) {
+            const arrayErrors = JSON.parse(error.request.response);
+            const listErrors = {};
+            if (arrayErrors.total == null) {
+              listErrors.push(arrayErrors.message);
+            } else {
+              arrayErrors._embedded.errors.forEach((element) => {
+                listErrors.push(element.message);
+              });
+            }
+            alert('Erro no registo', listErrors, null, null, null);
+          } else if (error.request) {
+            alert('Erro no registo', error.request, null, null, null);
+          } else {
+            alert('Erro no registo', error.message, null, null, null);
+          }
+        });
+    }
+  },
+  patch(id: number, params: string) {
+    return api()
+      .patch('identifierType/' + id, params)
+      .then((resp) => {
+        identifierType.save(resp.data);
+        alert(
+          'Sucesso!',
+          'O Registo foi alterado com sucesso',
+          null,
+          null,
+          null
+        );
+      })
+      .catch((error) => {
+        if (error.request != null) {
+          const arrayErrors = JSON.parse(error.request.response);
+          const listErrors = {};
+          if (arrayErrors.total == null) {
+            listErrors.push(arrayErrors.message);
+          } else {
+            arrayErrors._embedded.errors.forEach((element) => {
+              listErrors.push(element.message);
+            });
+          }
+          alert('Erro no registo', listErrors, null, null, null);
+        } else if (error.request) {
+          alert('Erro no registo', error.request, null, null, null);
+        } else {
+          alert('Erro no registo', error.message, null, null, null);
+        }
+      });
+  },
+  delete(id: number) {
+    return api()
+      .delete('identifierType/' + id)
+      .then(() => {
+        identifierType.destroy(id);
+      });
+  },
+};
