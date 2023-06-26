a955036dd72ed221209a54966c523f861c193138
muhammadchona <muhammadchona@gmail.com>
2023-06-26 23:21:06 +0200

    classes para criacao de tabelas da base de dados mobile

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,8 @@ import { store } from 'quasar/wrappers';
 import { createPinia } from 'pinia';
 import { Router } from 'vue-router';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import NonSqlDatabaseUtils from 'src/utils/NonSqlDatabaseUtils';
+import nanosSqlDatabase from 'src/stores/nanosSqlDatabase';
 /*
  * When adding new properties to stores, you should also
  * extend the `PiniaCustomProperties` interface.
@@ -24,7 +26,9 @@ declare module 'pinia' {
 
 export default store((/* { ssrContext } */) => {
   const pinia = createPinia();
-
+  const list = nanosSqlDatabase.getEntities();
+  console.log(list);
+  NonSqlDatabaseUtils.initDatabase(list);
   // You can add Pinia plugins here
   // pinia.use(SomePiniaPlugin)
   pinia.use(piniaPluginPersistedstate);
---
+++ src/utils/NonSqlDatabaseUtils.ts
@@ -0,0 +1,72 @@
+import { nSQL } from 'nano-sql';
+import { getMode } from 'cordova-plugin-nano-sqlite/lib/sqlite-adapter';
+
+export default {
+  createModel(fields: any, entity: any) {
+    const listaFields = [];
+    for (const key in fields) {
+      let dataType = 'date';
+      const valor = Object.getPrototypeOf(fields[key]).constructor.name;
+      if (key === 'weeks') {
+        console.log(Object.getPrototypeOf(fields[key]).constructor.name);
+      }
+      switch (valor) {
+        case 'BelongsTo':
+          dataType = 'object';
+          break;
+        case 'HasMany':
+          dataType = 'array';
+          break;
+        case 'BelongsToMany':
+          dataType = 'array';
+          break;
+        case 'Attr':
+          dataType = 'string';
+          break;
+        case 'Boolean':
+          dataType = 'boolean';
+          break;
+        case 'Number':
+          dataType = 'int';
+          break;
+        default:
+          dataType = 'string';
+          break;
+      }
+      let obj = {};
+      if (key === 'id') {
+        obj = { key: key, type: 'uuid', props: ['pk'] };
+      } else if (
+        (key === 'drug_id' && entity === 'therapeuticRegimensDrug') ||
+        (key === 'therapeutic_regimen_id' &&
+          entity === 'therapeuticRegimensDrug')
+      ) {
+        console.log(key);
+        obj = { key: key, type: 'uuid', props: ['pk'] };
+      } else {
+        obj = { key: key, type: dataType };
+      }
+      listaFields.push(obj);
+    }
+    return listaFields;
+  },
+  async initDatabase(entityList: any) {
+    try {
+      const MyDb = nSQL();
+      for (let i = 0; i < entityList.length; i++) {
+        const model = this.createModel(
+          entityList[i].fields(),
+          entityList[i].entity
+        );
+        MyDb.table(entityList[i].entity)
+          .config({
+            mode: getMode(),
+          })
+          .model(model);
+      }
+      await Promise.all([MyDb.connect()]);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
