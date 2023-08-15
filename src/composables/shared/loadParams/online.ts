77f7c85242b8af5b897ad0b69ead500861a75d75
miliate <erciliomiliate@gmail.com>
2023-08-15 12:44:15 +0200

    fix stock data loading

--- src/composables/shared/loadParams/online.ts
+++ src/composables/shared/loadParams/online.ts
@@ -47,14 +47,6 @@ import menuService from 'src/services/api/menu/menuService';
 import roleMenuService from 'src/services/api/roleMenu/roleMenuService';
 import userService from 'src/services/api/user/userService';
 
-import StockService from 'src/services/api/stockService/StockService';
-
-import ReferedStockMovimentService from 'src/services/api/referedStockMovimentService/ReferedStockMovimentService';
-import InventoryStockAdjustmentService from 'src/services/api/stockAdjustment/InventoryStockAdjustmentService';
-
-import InventoryService from 'src/services/api/inventoryService/InventoryService';
-import DestroyedStockService from 'src/services/api/destroyedStockService/DestroyedStockService';
-import StockAlertService from 'src/services/api/stockAlertService/StockAlertService';
 
 export function useOnline() {
   function loadSettingParams() {
@@ -87,46 +79,17 @@ export function useOnline() {
     provinceService.get(0);
     districtService.get(0);
     clinicService.get(0);
-    stockService.get(0);
-    StockEntranceService.get(0);
     roleService.get(0);
     menuService.get(0);
     userService.get(0);
-    // roleMenuService.get(0);
-    // patientService.get(0);
-    // episodeService.get(0);
     StockOperationTypeService.get(0);
-    //  patientVisitService.get(0);
-    //   patientVisitDetailsService.apiGetAllLastOfClinic(clinic.value.id, 0, 1000);
-    //   packService.apiGetAllLastOfClinic(clinic.value.id, 0, 1000);
-    //   prescriptionService.apiGetAllLastOfClinic(clinic.value.id, 0, 1000);
-    // prescribedDrugService.get(0);
-    // packagedDrugService.get(0);
+  
 
-    StockService.get(0);
-    ReferedStockMovimentService.get(0);
-    DestroyedStockService.get(0);
-    StockEntranceService.get(0)
-    InventoryStockAdjustmentService.get(0);
-    InventoryService.get(0);
-    StockEntranceService.get(0);
 
 
   }
 
   function loadPatientData() {
-    /* patientService.get(0);
-    patientAttributeService.get(0);
-    patientServiceIdentifierService.get(0);
-    patientVisitService.get(0);
-    patientVisitDetailsService.get(0);
-    episodeService.get(0);
-    packService.get(0);
-    prescriptionService.get(0);
-    prescriptionDetailsService.get(0);
-    packagedDrugService.get(0);
-    prescribedDrugService.get(0);
-    */
     patientService.doPatientsBySectorGet();
     episodeService.doEpisodesBySectorGet();
   }
--- src/pages/Stock/Stock.vue
+++ src/pages/Stock/Stock.vue
@@ -7,12 +7,24 @@
 <script setup>
 
 
-import { onMounted } from 'vue';
+import { onBeforeMount, onMounted } from 'vue';
 
 import Index from 'src/components/Stock/Index.vue';
 
 import StockAlertService from 'src/services/api/stockAlertService/StockAlertService';
 import clinicService from 'src/services/api/clinicService/clinicService';
+import StockService from 'src/services/api/stockService/StockService';
+
+import ReferedStockMovimentService from 'src/services/api/referedStockMovimentService/ReferedStockMovimentService';
+import InventoryStockAdjustmentService from 'src/services/api/stockAdjustment/InventoryStockAdjustmentService';
+
+import InventoryService from 'src/services/api/inventoryService/InventoryService';
+import DestroyedStockService from 'src/services/api/destroyedStockService/DestroyedStockService';
+import StockEntranceService from 'src/services/api/stockEntranceService/StockEntranceService';
+import { useLoading } from 'src/composables/shared/loading/loading';
+
+
+const { closeLoading, showloading } = useLoading();
 
 
 
@@ -20,6 +32,17 @@ onMounted(() => {
   const clinic = clinicService.currClinic()
   StockAlertService.apiGetStockAlertAll( clinic.id)
 });
+
+onBeforeMount(() => {
+  showloading()
+    StockService.get(0);
+    ReferedStockMovimentService.get(0);
+    DestroyedStockService.get(0);
+    StockEntranceService.get(0)
+    InventoryStockAdjustmentService.get(0);
+    InventoryService.get(0);
+    StockEntranceService.get(0);
+})
 
 
 </script>
