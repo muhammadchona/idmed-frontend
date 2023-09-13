922978ee4b46f38003518bd88aecb35cec87ac28
colaconhongo <colaco.nhango@fgh.org.mz>
2023-09-13 09:39:04 +0200

    Update env File
    Set Env File to be visible

--- quasar.config.js
+++ quasar.config.js
@@ -74,7 +74,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
-      env: require('dotenv').config().parsed,
+      env: require('dotenv').config({ override: true }).parsed,
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16',
